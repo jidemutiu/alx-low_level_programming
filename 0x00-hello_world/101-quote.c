0d380c26b6e30812af830713a9e2ae3bffa609b0
jidemutiu <jidenamuslim@gmail.com>
2023-02-18 10:03:51 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that print  line without using puts and printf functions.
- The program add a newline.
+ * main - A program that print a line to the standard error.
  * Return 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
