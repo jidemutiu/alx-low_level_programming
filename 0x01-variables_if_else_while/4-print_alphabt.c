57432ae05b815541daef2cf037bab7d5438dc310
jidemutiu <jidenamuslim@gmail.com>
2023-02-20 04:19:05 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Program that alphabets in lower case and exclude some alphabtes.
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char lo, e, q;
+	e = 'e';
+	q = 'q';
+
+	for (lo = 'a'; lo <= 'z'; lo++)
+	{
+	if (lo != e && lo != q)
+	putchar(lo);
+	}
+	putchar('\n');
+	return (0);
+}
+
