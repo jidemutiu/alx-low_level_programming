cfaae1dfb6ba62def89b1819374a231416f9b13e
jidemutiu <jidenamuslim@gmail.com>
2023-02-20 04:35:57 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Program to print number betwee 0 -10.
+*Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	printf("%d", i);
+	printf("\n");
+	return (0);
+}
