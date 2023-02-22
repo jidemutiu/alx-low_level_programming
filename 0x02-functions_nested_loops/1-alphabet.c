efeeadfda702da3ffe2da65f179d31d3d6b485cc
jidemutiu <jidenamuslim@gmail.com>
2023-02-22 07:53:06 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main- program that prints alphabets and make the alphabets
+ * Return void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
