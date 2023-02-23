9f9fd4c06f341c0d4994246958f1d9501963d747
jidemutiu <jidenamuslim@gmail.com>
2023-02-23 06:47:39 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isupper - check for uppercase letter.
+ *@c:character to check
+ *Return: 0 Always
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c >= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
