8b70aacf2f321619bd5b3a33ca9f2a79e5f39fa3
jidemutiu <jidenamuslim@gmail.com>
2023-02-23 09:17:33 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multily to integers
+ * @a: the first integer number
+ * @b: the second integer number
+ * Return: 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
