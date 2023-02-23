4f1f9eaa20bf7a65d40f38d02c42e3a5bb5435ff
jidemutiu <jidenamuslim@gmail.com>
2023-02-22 16:56:42 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*print_sign - check sing of a number.
+*@n: tThe number to be checked
+*Return: 1 for positive, 0 for zero and -1 for negative
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
