d806463be5b25fddeb71c9fbcb5dbb666924d7db
jidemutiu <jidenamuslim@gmail.com>
2023-03-04 10:56:08 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,7 @@
  * Return:  A pointer to the changed word
  */
 
- char *cap_string(char *str)
+char *cap_string(char *str)
 {
 	int index = 0;
 
@@ -14,19 +14,19 @@
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-		if (str[index -1] == ' ' ||
-		str[index - 1] == '\t'||
-		str[index - 1] == '\n'||
-		str[index - 1] == ','||
-		str[index - 1] == ';'||
-		str[index - 1] == '.'||
-		str[index - 1] == '!'||
-		str[index - 1] == '?'||
-		str[index - 1] == '"'||
-		str[index - 1] == '('||
-		str[index - 1] == ')'||
-		str[index - 1] == '{'||
-		str[index - 1] == '}'||
+		if (str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
 		str[index] == 0)
 			str[index] -= 32;
 		index++;
