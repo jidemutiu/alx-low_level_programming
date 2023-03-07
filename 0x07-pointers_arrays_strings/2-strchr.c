9473e1e0a45b0030e1a36f1e04a03cbb43c7b7ab
jidemutiu <jidenamuslim@gmail.com>
2023-03-06 16:55:40 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: input value
+ * @c: input value
+ * Return: 0 (success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
