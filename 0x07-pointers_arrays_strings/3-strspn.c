993034171a54d088485afe3df464b5a9b8768a84
jidemutiu <jidenamuslim@gmail.com>
2023-03-06 17:22:59 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - grt the lenth of prefix of  substring
+ * @s: input
+ * @accept: input
+ * Return: 0 (succes)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, n, value, check;
+
+	value = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		check = 0;
+
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[i])
+				value++;
+				check = 1;
+		}
+	}
+}
